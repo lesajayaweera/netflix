3af44960e7d8bb2c14f53e3c71e7c5f14c5e1c69
lesajayaweera <166988125+lesajayaweera@users.noreply.github.com>
2024-11-24 07:18:58 +0530

    starting the Featured part in the home page

--- src/Compornents/Featured/Featured.js
+++ src/Compornents/Featured/Featured.js
@@ -1,12 +1,13 @@
 import './Featured.css';
-
 import { React } from 'react';
-import FeaturedImg from "../../images/deadpool-wolverine.jpg";
+import FeaturedImg from "../../images/FeaturedImages/deadpool-wolverine.jpg";
+
 const Featured =()=>{
     return (
       <div className="Featured">
         <img className="featured-image" src={FeaturedImg} alt="Featured-img"/>
         <div className="info">
+         
               
         </div>
       </div>
